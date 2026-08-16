14a6574587cdcdebef7096ca159a5a14e7347f8f
Mahaoui Abdennasser <nasrosoft.dev@gmail.com>
2026-08-17 00:08:52 +0100

    Restrict delivery_items DELETE policy to admin only

--- fix_delivery_items_rls.sql
+++ fix_delivery_items_rls.sql
@@ -15,10 +15,6 @@ CREATE POLICY "Delivery can update assigned delivery_items" ON delivery_items FO
     )
 );
 
-CREATE POLICY "Delivery can delete assigned delivery_items" ON delivery_items FOR DELETE USING (
-    EXISTS (
-        SELECT 1 FROM delivery_orders 
-        WHERE delivery_orders.id = delivery_items.delivery_order_id 
-        AND (delivery_orders.delivery_employee_id = auth.uid() OR is_admin())
-    )
+CREATE POLICY "Admin can delete delivery_items" ON delivery_items FOR DELETE USING (
+    is_admin()
 );
